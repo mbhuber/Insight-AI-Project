12a54a6fd3978593b2038549cd99d221c191dd6b
mbhuber@yahoo.com <mbhuber@yahoo.com>
2017-03-23 11:48:00 -0400

    faces

--- keras/keras_ac-gan_faces.py
+++ keras/keras_ac-gan_faces.py
@@ -206,7 +206,7 @@ if __name__ == '__main__':
 	fake = generator([latent, image_class])
 
 	# we only want to be able to train generation for the combined model
-	discriminator.trainable = False
+	discriminator.trainable = True #False
 	fake, aux = discriminator(fake)
 	combined = Model(input=[latent, image_class], output=[fake, aux])
 
